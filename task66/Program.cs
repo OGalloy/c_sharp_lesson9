214d8d3081faa65bb2ce7580a819705c42c17378
alloy <onedayaslion@gmail.com>
2022-07-22 15:45:20 +0500

    edited task66

---
+++ task66/Program.cs
@@ -0,0 +1,24 @@
+﻿/* 
+Задача 66: Задайте значения M и N. 
+Напишите программу, которая найдёт сумму натуральных 
+элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int SumNumbers(int m, int n)
+{
+    if(m <= 0 || n <= 0)
+    {
+        return -1;
+    }
+    else //error
+    {
+    if (m == n) return  n;
+        return m + SumNumbers(m + 1, n);
+    }
+}
+
+Console.WriteLine(SumNumbers(3, 6));
+//output 18
+
