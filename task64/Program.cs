9a776e24197170e4f32faba5b73f0dd7a0ee5012
alloy <onedayaslion@gmail.com>
2022-07-22 15:04:35 +0500

    init, add .gitignore and task64

---
+++ task64/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 64: Задайте значения M и N. 
+Напишите программу, которая выведет все натуральные числа 
+в промежутке от M до N.
+M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+M = 4; N = 8. -> ""4, 6, 7, 8""*/
+//
+string PrintNumbers(int m, int n)
+{   
+    if (m <= 0 || n <=0)
+    {
+       return "Один или более аргументов не являются натуральными числами"; 
+    }
+    else
+    {
+        if (m == n) return n.ToString();
+        return m.ToString() + PrintNumbers(m+1, n);
+    }
+}
+
+Console.WriteLine(PrintNumbers(5, 15));
